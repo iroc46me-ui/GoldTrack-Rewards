dce52e56167df0dc0ef21c716e5deecab6a58fe8
iroc46me-ui <iroc46me@gmail.com>
2026-06-21 11:50:15 -0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "GoldTrack Rewards",
+  description: "Our Community. Our Initiatives. Our Legacy."
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
