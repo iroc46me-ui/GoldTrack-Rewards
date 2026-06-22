def9ace1acc11307c3e45c8250b30200a62ffd4e
iroc46me-ui <iroc46me@gmail.com>
2026-06-22 08:23:38 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,12 @@ export default function HomePage() {
         src="/thumbnail.jpg"
         alt="The Door Is Open"
         style={{
-           width: "70%",
-  maxWidth: "700px",
-  margin: "0 auto",
-  display: "block"
-}}
+          width: "70%",
+          maxWidth: "700px",
+          margin: "0 auto",
+          display: "block",
+        }}
+      />
+    </main>
+  );
+}
