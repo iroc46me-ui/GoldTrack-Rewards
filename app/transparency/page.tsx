4b2a890ac0fd2a9767a1bd59f8ecaa8791a5e9f4
iroc46me-ui <iroc46me@gmail.com>
2026-06-23 13:04:28 -0700

    Update page.tsx

--- app/transparency/page.tsx
+++ app/transparency/page.tsx
@@ -1,19 +1,3 @@
-import Image from "next/image";
-
-export default function TransparencyPage() {
-  return (
-    <main className="min-h-screen bg-black">
-      <Image
-        src="/thumbnail.jpg"
-        alt="Transparency Archive"
-        width={1536}
-        height={4096}
-        className="w-full h-auto"
-        priority
-      />
-    </main>
-  );
-}
 
 export default function TransparencyPage() {
   return (
