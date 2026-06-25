ae4bdae34a1d96b70e9c447eec4c05ec8f2bda4e
iroc46me-ui <iroc46me@gmail.com>
2026-06-25 12:58:34 -0700

    Update page.tsx

--- app/ideas/page.tsx
+++ app/ideas/page.tsx
@@ -2,7 +2,7 @@ export default function IdeasPage() {
   return (
     <main style={{ margin: 0, background: "#061827", minHeight: "100vh" }}>
       <img
-        src="/ideas-and-innovation.jpg"
+        src="/ideas-and-inovation.jpg"
         alt="Ideas and Innovation"
         style={{
           width: "100%",
