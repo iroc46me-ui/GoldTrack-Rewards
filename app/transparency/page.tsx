fa1f1f00caa21493da173754e4d86884d5b2abb6
iroc46me-ui <iroc46me@gmail.com>
2026-06-29 11:32:16 -0700

    Update page.tsx

--- app/transparency/page.tsx
+++ app/transparency/page.tsx
@@ -68,7 +68,7 @@ export default function TransparencyPage() {
   style={{
     position: "absolute",
     left: "50.2%",
-    top: "17.5%",
+    top: "17%",
     width: "20.2%",
     height: "3%",
     cursor: "pointer",
