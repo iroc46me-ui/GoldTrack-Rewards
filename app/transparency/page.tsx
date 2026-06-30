7dcd2cc577aa3024ac75888f9b1c224583e22d1b
iroc46me-ui <iroc46me@gmail.com>
2026-06-29 17:27:47 -0700

    Update page.tsx

--- app/transparency/page.tsx
+++ app/transparency/page.tsx
@@ -67,7 +67,7 @@ export default function TransparencyPage() {
   aria-label="Community History"
   style={{
     position: "absolute",
-    left: "54%",
+    left: "51%",
     top: "17%",
     width: "20.2%",
     height: "3%",
